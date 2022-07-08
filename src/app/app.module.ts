b65fe021eb7835eb74da040220be53107be1df93
ajalkrishna <ajalkrishnav@gmail.com>
2022-07-08 12:35:48 +0530

    'first-commit'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { IntroComponent } from './intro/intro.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
+import { SectionHeadComponent } from './section-head/section-head.component';
+import { SkillComponent } from './skill/skill.component';
+import { EducationComponent } from './education/education.component';
+import { AchievementComponent } from './achievement/achievement.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PortfolioComponent,
+    NavbarComponent,
+    IntroComponent,
+    AboutmeComponent,
+    SectionHeadComponent,
+    SkillComponent,
+    EducationComponent,
+    AchievementComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
